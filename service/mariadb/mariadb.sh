4435bde9aa889815085f6aac6d5abea60c849a3d
Bertrand Gouny <bertrand.gouny@osixia.fr>
2014-07-16 19:52:22 +0200

    debian-user

--- service/mariadb/mariadb.sh
+++ service/mariadb/mariadb.sh
@@ -6,15 +6,16 @@ status () {
   echo "---> ${@}" >&2
 }
 
-ROOT_USER=${ROOT_USER}
-ROOT_PWD=${ROOT_PWD}
+set -x
+: ROOT_USER=${ROOT_USER}
+: ROOT_PWD=${ROOT_PWD}
+
+# Start MariaDB
+service mysql start
 
 ############ Base config ############
 if [ ! -e /var/lib/mysql/docker_bootstrapped ]; then
   status "configuring MariaDB database"
-  
-  # Start MariaDB
-  service mysql start
 
   # Run mysql_secure_installation
   expect config/config.exp
@@ -26,12 +27,22 @@ if [ ! -e /var/lib/mysql/docker_bootstrapped ]; then
   mysql -u root -p$ROOT_PWD -e "UPDATE mysql.user SET user='${ROOT_USER}' WHERE user='root';"
   mysql -u root -p$ROOT_PWD -e 'FLUSH PRIVILEGES;'
 
-  service mysql stop
-
   touch /var/lib/mysql/docker_bootstrapped
 
 else
   status "MariaDB database found"
 fi
+
+# The password for 'debian-sys-maint'@'localhost' is auto generated.
+# The database inside of DATA_DIR may not have been generated with this password.
+# So, we need to set this for our database to be portable.
+# https://github.com/Painted-Fox/docker-mariadb/blob/master/scripts/first_run.sh
+
+DB_MAINT_PASS=$(cat /etc/mysql/debian.cnf | grep -m 1 "password\s*=\s*"| sed 's/^password\s*=\s*//')
+
+mysql -u $ROOT_USER -p$ROOT_PWD -e "UPDATE mysql.user SET Password=PASSWORD('$DB_MAINT_PASS') WHERE User='debian-sys-maint';"
+mysql -u $ROOT_USER -p$ROOT_PWD -e 'FLUSH PRIVILEGES;'
+
+service mysql stop
 
 exec /usr/bin/mysqld_safe
