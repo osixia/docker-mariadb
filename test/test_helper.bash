7f2858b32a1738d7baf387b20a0a95b449af1963
Bertrand Gouny <bgouny@hi-media.com>
2015-02-23 14:43:57 +0100

    tests

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,7 +1,8 @@
 setup() {
   IMAGE_NAME="$NAME:$VERSION"
 }
-  
+
+# function relative to the current container / image  
 build_image() {
   #disable outputs
   docker build -t $IMAGE_NAME $BATS_TEST_DIRNAME/../image &> /dev/null
@@ -9,45 +10,91 @@ build_image() {
 
 run_image() {
   CONTAINER_ID=$(docker run $@ -d $IMAGE_NAME)
-  CONTAINER_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $CONTAINER_ID)
+  CONTAINER_IP=$(get_container_ip_by_cid $CONTAINER_ID)
 }
 
 start_container() {
-  #disable outputs
-  docker start $CONTAINER_ID &> /dev/null
+  start_containers_by_cid $CONTAINER_ID
 }
 
 stop_container() {
-  #disable outputs
-  docker stop $CONTAINER_ID &> /dev/null
+  stop_containers_by_cid $CONTAINER_ID
 }
 
 remove_container() {
-  #disable outputs
- docker rm $CONTAINER_ID &> /dev/null
+ remove_containers_by_cid $CONTAINER_ID
 }
 
 clear_container() {
-  stop_container
-  remove_container
+  stop_containers_by_cid $CONTAINER_ID
+  remove_containers_by_cid $CONTAINER_ID
 }
 
 is_service_running() {
-  docker exec $CONTAINER_ID ps cax | grep $1  > /dev/null
+  is_service_running_by_cid $CONTAINER_ID $1
 }
 
 wait_service() {
-  
+  wait_service_by_cid $CONTAINER_ID $@
+}
+
+
+# generic functions 
+get_container_ip_by_cid() {
+  local IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $1)
+  echo "$IP"
+}
+
+start_containers_by_cid() {
+  for cid in "$@"
+  do
+    #disable outputs
+    docker start $cid &> /dev/null
+  done 
+}
+
+stop_containers_by_cid() {
+  for cid in "$@"
+  do
+    #disable outputs
+    docker stop $cid &> /dev/null
+  done 
+}
+
+remove_containers_by_cid() {
+  for cid in "$@"
+  do
+    #disable outputs
+    docker rm $cid &> /dev/null
+  done 
+}
+
+clear_containers_by_cid() {
+  stop_containers_by_cid $@
+  remove_containers_by_cid $@
+}
+
+is_service_running_by_cid() {
+  docker exec $1 ps cax | grep $2  > /dev/null
+}
+
+wait_service_by_cid() {
+
+  cid=$1
+
   # first wait image init end
-  while ! is_service_running syslog-ng
+  while ! is_service_running_by_cid $cid syslog-ng
   do
     sleep 1
   done
 
-  # wait service
-  while ! is_service_running $1
+  for service in "${@:2}"
   do
-    sleep 1
+    # wait service
+    while ! is_service_running_by_cid $cid $service
+    do
+      sleep 1
+    done
   done
 
   sleep 5
